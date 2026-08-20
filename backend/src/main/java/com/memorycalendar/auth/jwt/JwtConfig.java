90c61dd3c0e5fdd9828e201b253977aa589a6d07
sunghyun <csh980116@naver.com>
2026-08-20 20:59:10 +0900

    feat: JWT Access Token 인증 적용

--- backend/src/main/java/com/memorycalendar/auth/jwt/JwtConfig.java
+++ backend/src/main/java/com/memorycalendar/auth/jwt/JwtConfig.java
@@ -3,7 +3,10 @@ package com.memorycalendar.auth.jwt;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
 import javax.crypto.SecretKey;
@@ -15,15 +18,25 @@ import java.nio.charset.StandardCharsets;
 public class JwtConfig {
 
     @Bean
-    public JwtEncoder jwtEncoder(JwtProperties properties) {
-
-        SecretKey secretKey = new SecretKeySpec(
-                properties.secret().getBytes(StandardCharsets.UTF_8)
-                , "HmacSHA256"
+    public SecretKey jwtSecretKey(JwtProperties properties) {
+        return new SecretKeySpec(
+                properties.secret().getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
         );
+    }
 
+    @Bean
+    public JwtEncoder jwtEncoder(SecretKey secretKey) {
         return NimbusJwtEncoder
                 .withSecretKey(secretKey)
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(SecretKey secretKey) {
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
                 .build();
     }
 }
--- backend/src/main/java/com/memorycalendar/global/common/config/SecurityConfig.java
+++ backend/src/main/java/com/memorycalendar/global/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.memorycalendar.global.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +24,10 @@ public class SecurityConfig {
                                 "/api/health"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
                 );
 
         return http.build();
